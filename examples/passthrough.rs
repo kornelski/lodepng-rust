4d54d22a2396d46e5912c12da4b403ba748aae75
Kornel Lesiński <kornel@geekhood.net>
2014-11-08 17:06:15 +0000

    Updated to Rust 0.13 nightly

--- examples/passthrough.rs
+++ examples/passthrough.rs
@@ -2,11 +2,11 @@ extern crate lodepng;
 
 fn main() {
     let path = &Path::new("test.png");
-    
+
     // Since we're using decode24_file, we get an RGB bitmap
     let bitmap = match lodepng::decode24_file(path) {
         Ok(bitmap) => bitmap,
-        Err(reason) => fail!("Could not load {}, because: {}", path.display(), reason),
+        Err(reason) => panic!("Could not load {}, because: {}", path.display(), reason),
     };
 
     let path = &Path::new("write_test.png");
@@ -14,7 +14,7 @@ fn main() {
     let buffer = bitmap.buffer.as_slice();
     // Now we reencode it, using LCT_RGB since we used decode24_file
     match lodepng::encode_file(path, buffer, bitmap.width, bitmap.height, lodepng::LCT_RGB, 8) {
-        Err(e) => fail!("failed to write png: {}", e),
+        Err(e) => panic!("failed to write png: {}", e),
         Ok(_) => (),
     }
 }
