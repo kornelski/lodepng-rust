fa2247abfcf2a3fc1ffc8fbc54e1214cb9f91de9
Kornel Lesiński <kornel@geekhood.net>
2015-01-20 07:26:14 +0000

    Updated examples

--- examples/passthrough.rs
+++ examples/passthrough.rs
@@ -1,20 +1,22 @@
 extern crate lodepng;
 
 fn main() {
-    let path = &Path::new("test.png");
+    let from_path = &Path::new("test.png");
 
     // Since we're using decode24_file, we get an RGB bitmap
-    let bitmap = match lodepng::decode24_file(path) {
+    let bitmap = match lodepng::decode24_file(from_path) {
         Ok(bitmap) => bitmap,
-        Err(reason) => panic!("Could not load {}, because: {}", path.display(), reason),
+        Err(reason) => panic!("Could not load {}, because: {}", from_path.display(), reason),
     };
 
-    let path = &Path::new("write_test.png");
+    let to_path = &Path::new("write_test.png");
 
     let buffer = bitmap.buffer.as_slice();
+
     // Now we reencode it, using LCT_RGB since we used decode24_file
-    match lodepng::encode_file(path, buffer, bitmap.width, bitmap.height, lodepng::LCT_RGB, 8) {
-        Err(e) => panic!("failed to write png: {}", e),
-        Ok(_) => (),
+    if let Err(e) = lodepng::encode_file(to_path, buffer, bitmap.width, bitmap.height, lodepng::LCT_RGB, 8) {
+        panic!("failed to write png: {}", e);
     }
+
+    println!("Copied {} to {}", from_path.display(), to_path.display());
 }
--- examples/write.rs
+++ examples/write.rs
@@ -11,8 +11,9 @@ fn main() {
 
     // encode_file takes the path to the image, a u8 array,
     // the width, the height, the color mode, and the bit depth
-    match lodepng::encode_file(path, &image, 2, 2, lodepng::LCT_RGB, 8) {
-        Err(e) => panic!("failed to write png: {}", e),
-        Ok(_) => (),
-   }
+    if let Err(e) = lodepng::encode_file(path, &image, 2, 2, lodepng::LCT_RGB, 8) {
+        panic!("failed to write png: {}", e);
+    }
+
+    println!("Written to {}", path.display());
 }
