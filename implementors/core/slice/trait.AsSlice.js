11c72c09db05689aca3d71c2ce31a019dbabfbb7
Kornel Lesiński <kornel@geekhood.net>
2015-01-22 01:44:09 +0000

    Docs for 0.3

---
+++ implementors/core/slice/trait.AsSlice.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['c_vec'] = ["<a class='stability Unstable' title='Unstable: will be replaced by slice syntax'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/slice/trait.AsSlice.html' title='core::slice::AsSlice'>AsSlice</a>&lt;T&gt; for <a class='struct' href='c_vec/struct.CVec.html' title='c_vec::CVec'>CVec</a>&lt;T&gt;",];
+implementors['lodepng'] = ["<a class='stability Unstable' title='Unstable: will be replaced by slice syntax'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/slice/trait.AsSlice.html' title='core::slice::AsSlice'>AsSlice</a>&lt;T&gt; for <a class='struct' href='lodepng/ffi/struct.CVec.html' title='lodepng::ffi::CVec'>CVec</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
