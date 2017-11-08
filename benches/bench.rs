55c981b2403327d51e45433d2751eeb91ed273ce
Kornel <kornel@geekhood.net>
2017-11-08 12:25:56 +0000

    No-file bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,8 +4,11 @@ extern crate lodepng;
 
 #[bench]
 fn encode(bencher: &mut test::Bencher) {
-    let img = lodepng::decode24_file("/tmp/test.png").unwrap();
+    let mut data = vec![0u8; 640*480*3];
+    for (i, px) in data.iter_mut().enumerate() {
+        *px = ((i ^ (13 + i * 17) ^ (i * 13) ^ (i/113 * 11)) >> 5) as u8;
+    }
     bencher.iter(|| {
-        lodepng::encode_memory(&img.buffer, img.width, img.height, lodepng::ColorType::RGB, 8).unwrap()
+        lodepng::encode_memory(&data, 640, 480, lodepng::ColorType::RGB, 8).unwrap()
     });
 }
