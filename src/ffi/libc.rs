b10ce55e2692ba222f4f403d80d7bae519acf143
Nicolas Mattia <nicolas@nmattia.com>
2021-11-12 12:12:07 +0100

    Add wasm32 implementation for libc
    This adds a wasm32 implementation for `free`, `malloc` and `realloc`.
    Originally written by @rodrigorc in
    https://github.com/rust-lang/libc/pull/1092 and adapted.
    
    NOTE: This is only enabled for `wasm32-unknown-unknown` ([`target_arch =
    "wasm32"`](https://doc.rust-lang.org/reference/conditional-compilation.html#target_arch), [`target_os =
    "unknown"`](https://doc.rust-lang.org/reference/conditional-compilation.html#target_os)), because other wasm targets (like `wasm32-unknown-emscripten`) may provide a `libc` implementation.

---
+++ src/ffi/libc.rs
@@ -0,0 +1,36 @@
+pub type size_t = usize;
+
+const MALLOC_HEADER : isize = 8;
+const MALLOC_ALIGN : usize = 8;
+
+use super::c_void;
+use std::alloc::{self, Layout};
+use std::ptr;
+
+pub unsafe fn malloc(size: size_t) -> *mut c_void {
+    let lay = Layout::from_size_align_unchecked(MALLOC_HEADER as usize + size, MALLOC_ALIGN);
+    let p = alloc::alloc(lay);
+    if p.is_null() {
+        return ptr::null_mut();
+    }
+    *(p as *mut size_t) = size;
+    p.offset(MALLOC_HEADER) as *mut c_void
+}
+pub unsafe fn free(p: *mut c_void) {
+    let p = p.offset(-MALLOC_HEADER) as *mut u8;
+    let size = *(p as *mut size_t);
+    let lay = Layout::from_size_align_unchecked(MALLOC_HEADER as usize + size, MALLOC_ALIGN);
+    alloc::dealloc(p, lay);
+}
+pub unsafe fn realloc(p: *mut c_void, _size: size_t) -> *mut c_void {
+    let p = p.offset(-MALLOC_HEADER) as *mut u8;
+    let size = *(p as *mut size_t);
+    let lay = Layout::from_size_align_unchecked(MALLOC_HEADER as usize + size, MALLOC_ALIGN);
+    let p = alloc::realloc(p, lay, size);
+    if p.is_null() {
+        return ptr::null_mut();
+    }
+    *(p as *mut size_t) = size;
+    p.offset(MALLOC_HEADER) as *mut c_void
+}
+
