2eca806cc8f7610b0018d409140f10b6e83d2387
Kornel <kornel@geekhood.net>
2021-11-30 01:36:09 +0000

    Correct offset by c_void and realloc size
    #51

--- src/ffi/libc.rs
+++ src/ffi/libc.rs
@@ -16,21 +16,23 @@ pub unsafe fn malloc(size: size_t) -> *mut c_void {
     *(p as *mut size_t) = size;
     p.offset(MALLOC_HEADER) as *mut c_void
 }
+
 pub unsafe fn free(p: *mut c_void) {
-    let p = p.offset(-MALLOC_HEADER) as *mut u8;
+    let p = (p as *mut u8).offset(-MALLOC_HEADER);
     let size = *(p as *mut size_t);
     let lay = Layout::from_size_align_unchecked(MALLOC_HEADER as usize + size, MALLOC_ALIGN);
     alloc::dealloc(p, lay);
 }
-pub unsafe fn realloc(p: *mut c_void, _size: size_t) -> *mut c_void {
-    let p = p.offset(-MALLOC_HEADER) as *mut u8;
+
+pub unsafe fn realloc(p: *mut c_void, new_size: size_t) -> *mut c_void {
+    let p = (p as *mut u8).offset(-MALLOC_HEADER);
     let size = *(p as *mut size_t);
     let lay = Layout::from_size_align_unchecked(MALLOC_HEADER as usize + size, MALLOC_ALIGN);
-    let p = alloc::realloc(p, lay, size);
+    let p = alloc::realloc(p, lay, new_size);
     if p.is_null() {
         return ptr::null_mut();
     }
-    *(p as *mut size_t) = size;
+    *(p as *mut size_t) = new_size;
     p.offset(MALLOC_HEADER) as *mut c_void
 }
 
