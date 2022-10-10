64e187b57da70b7b185870477ed753aecd41040e
Kornel <kornel@geekhood.net>
2022-10-10 21:02:18 +0100

    Bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,12 +2,13 @@
 extern crate test;
 
 #[bench]
-fn encode(bencher: &mut test::Bencher) {
+fn roundtrip(bencher: &mut test::Bencher) {
     let mut data = vec![0u8; 640*480*3];
     for (i, px) in data.iter_mut().enumerate() {
         *px = ((i ^ (13 + i * 17) ^ (i * 13) ^ (i/113 * 11)) >> 5) as u8;
     }
     bencher.iter(|| {
-        lodepng::encode_memory(&data, 640, 480, lodepng::ColorType::RGB, 8).unwrap()
+        let res = lodepng::encode_memory(&data, 640, 480, lodepng::ColorType::RGB, 8).unwrap();
+        lodepng::decode32(res)
     });
 }
